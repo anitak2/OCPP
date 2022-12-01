551e78c987c6a5aab89fd6861e1d4e307ef0aae4
anitak2 <32676541+anitak2@users.noreply.github.com>
2022-12-01 18:14:35 +0530

    Create http.js

---
+++ http.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const DB = require('../db/index.js');
+var Storage = new DB(process.env.storage);
+
+var CentralSystem = require('../entities/CentralSystem.js');
+var CentralSystemServer = new CentralSystem('9220');
+
+app.set('port', (process.env.PORT || 7000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send("Server is running..");
+});
+
+app.post('/api/v1/OCPP/bootNotification/:chargerID', (req, res) => {
+
+  console.log(`BootNotification Request success ${req.params.chargerID} & data is ${JSON.stringify(req.body)}`)
+  res.send({
+    "status": "Accepted",
+    "currentTime": "2022-03-25T02:41:13Z",
+    "interval": 600
+  }
+  ).status(200)
+
+});
+
+app.post('/api/v1/OCPP/statusNotification/:chargerID', (req, res) => {
+
+  console.log(`statusNotification Request success ${req.params.chargerID} & data is ${JSON.stringify(req.body)}`);
+  res.send({
+    messgae: "OK"
+  }
+  ).status(200)
+
+});
+
+app.post('/api/v1/OCPP/authorize/:chargerID', (req, res) => {
+
+  console.log(`statusNotification Request success ${req.params.chargerID} & data is ${JSON.stringify(req.body)}`)
+  res.send({
+    "idTagInfo": {
+      "status": "Accepted",
+      "expiryDate": "2022-04-01T11:41:13Z"
+    }
+
+  }
+  ).status(200)
+
+});
+
+var server = app.listen(app.get('port'), function () {
+  console.log('Server is running on port ' + app.get('port'));
+});
